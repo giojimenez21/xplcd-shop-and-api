b8d56ff8ee1fea38b91c2da8eed91351afce59ae
Gio Jiménez <gio.jimenezsj@gmail.com>
2022-10-31 11:10:00 -0600

    Se agrega cors.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 dotenv.config();
 
 import { routerProducts } from './routes';
@@ -7,7 +8,10 @@ import { routerProducts } from './routes';
 const PORT = process.env.PORT || 4000;
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
 
 app.use('/products', routerProducts);
 
